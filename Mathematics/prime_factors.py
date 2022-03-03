403d89b751967cd1d39be8cce6837c4e36d0375e
heyharshjaiswal <harshjaiswal2307@gmail.com>
2022-03-04 01:33:05 +0530

    prime_factors(v2.7)

---
+++ Mathematics/prime_factors.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+def primeFactors(n):
+    # Step 1 - Even 
+    while n % 2 == 0:
+        print(2)
+        n /= 2
+    # Step - 2 - Checking for Composite
+    for i in range(3, int(sqrt(n))+1, 2):
+        while n % i == 0:
+            print(i)
+            n /= i
+    # Step 3 Prime Numbers greater than 2
+    if n > 2:
+        print(int(n))
+
+n = 335
+primeFactors(n)

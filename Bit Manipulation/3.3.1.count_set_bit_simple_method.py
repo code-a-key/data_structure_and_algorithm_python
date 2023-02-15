2e25af55ed2f9028b3c89691d11abaa048fa981a
harshjaiswal <harshjaiswal2307@gmail.com>
2023-02-15 23:54:04 +0530

    count_set_bits

--- Bit Manipulation/3.3.1.count_set_bit_simple_method.py
+++ Bit Manipulation/3.3.1.count_set_bit_simple_method.py
@@ -6,13 +6,26 @@ Count Number of 1's (set bits) in the binary representation of a given integer.
 IP: n = 9 [1001]
 OP: 2
 
-TC:
-SC:
+TC: O(log n)
+SC: O(1)
 """
 def count_set_bits(num: int) -> int:
-    """Count set bits"""
+    """count set bits
 
+    Args:
+        num (int): input number
+
+    Returns:
+        int: number of set bits
+    """
+    count = 0
+    while num:
+        count += num & 1
+        num >>= 1
+    return count
+
+    # return bin(num).count('1')
 
 if __name__ == '__main__':
-    N = 9
-    count_set_bits(N)
+    N = 36
+    print(count_set_bits(N))
